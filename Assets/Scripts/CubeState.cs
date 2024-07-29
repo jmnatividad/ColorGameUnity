ecdcd063ccf780826e8b15fe473c350d75d458b6
gerdiedoo <gerd14435296@gmail.com>
2024-07-29 11:45:24 +0800

    detect upper side

---
+++ Assets/Scripts/CubeState.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeState : MonoBehaviour
+{
+    public string upperSide;
+    public int value;
+    public Vector3 state;
+    public int tolerance = 45;
+    public int getState () {
+        int iValue = -1;
+        Vector3 cube = this.transform.eulerAngles;
+
+        cube = new Vector3 (RoundToNearest(Mathf.RoundToInt (cube.x)),
+                            RoundToNearest(Mathf.RoundToInt (cube.y)), 
+                            RoundToNearest(Mathf.RoundToInt (cube.z)));
+        state = cube;
+        if (cube.x == 180 && cube.y == 270 ||
+            cube.x == 0 && cube.z == 90) {
+            iValue = 1; //orange
+        } else if (cube.x == 270) {
+            iValue = 2; //blue
+        } else if (cube.x == 180 && cube.z == 0 ||
+            cube.x == 0 && cube.z == 180) {
+            iValue = 3; // red
+        } else if (cube.x == 180 && cube.z == 180 ||
+            cube.x == 0 && cube.z == 0) {
+            iValue = 4; //green
+        } else if (cube.x == 90) {
+            iValue = 5; // yellow
+        } else if (cube.x == 0 && cube.z == 270 ||
+            cube.x == 180 && cube.z == 90) {
+            iValue = 6; // purple
+        }
+
+        upperSide = translateInt(iValue);
+        value = iValue;
+        return iValue;
+    }
+    public string translateInt(int x){
+        if(x == 1) return "orange";
+        if(x == 2) return "blue";
+        if(x == 3) return "red";
+        if(x == 4) return "green";
+        if(x == 5) return "yellow";
+        if(x == 6) return "purple";
+        return "";
+    }
+    float RoundToNearest(float value) {
+        
+        if (Mathf.Abs(value - 0) < tolerance) {
+            return 0;
+        } else if (Mathf.Abs(value - 90) < tolerance) {
+            return 90;
+        } else if (Mathf.Abs(value - 180) < tolerance) {
+            return 180;
+        } else if (Mathf.Abs(value - 270) < tolerance) {
+            return 270;
+        } else {
+            return value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getState();
+    }
+}
--- Assets/Scripts/ResetObjects.cs
+++ Assets/Scripts/ResetObjects.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Runtime.InteropServices;
+using TMPro;
 public class ResetObjects : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -11,9 +12,35 @@ public class ResetObjects : MonoBehaviour
     public bool reset = false;
     public bool play = false;
 
+    public int UpperSideTxt;
+    public TextMeshProUGUI cubeStates;
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+    [DllImport("__Internal")]
+    private static extern void HelloString(string str);
+
+    [DllImport("__Internal")]
+    private static extern void PrintFloatArray(float[] array, int size);
+
+    [DllImport("__Internal")]
+    private static extern int AddNumbers(int x, int y);
+
+    [DllImport("__Internal")]
+    private static extern string StringReturnValueFunction();
+
+    [DllImport("__Internal")]
+    private static extern void BindWebGLTexture(int texture);
+
+
     public float[]  possibleAngles = { -360, -270, -180, -90, 0, 90, 180, 270, 360 };
+
+    public void test(){
+        Hello();
+    }
     void Start()
     {
+
         foreach (GameObject obj in gameObjects)
         {
             if (obj != null)
@@ -22,18 +49,20 @@ public class ResetObjects : MonoBehaviour
             }
         }
         randomRoll();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // getState(gameObjects[1]);
+        // Debug.Log(gameObjects[0].GetComponent<CubeState>().upperSide);
+        cubeStates.text = gameObjects[0].GetComponent<CubeState>().upperSide + " " + gameObjects[1].GetComponent<CubeState>().upperSide + " " + gameObjects[2].GetComponent<CubeState>().upperSide;
     }
+
     public void randomRoll(){
         foreach (GameObject obj in gameObjects)
         {
-
-
             if (obj != null)
             {
                 int randomIndex = Random.Range(0, possibleAngles.Length);
@@ -57,5 +86,33 @@ public class ResetObjects : MonoBehaviour
             }
         }
     }
+    // public int getState (GameObject gameObject) {
+    //     int iValue = -1;
+    //     Vector3 cube = gameObject.transform.eulerAngles;
+
+    //     cube = new Vector3 (Mathf.RoundToInt (cube.x), Mathf.RoundToInt (cube.y), Mathf.RoundToInt (cube.z));
+
+    //     if (cube.x == 180 && cube.y == 270 ||
+    //         cube.x == 0 && cube.z == 90) {
+    //         iValue = 1; //orange
+    //     } else if (cube.x == 270) {
+    //         iValue = 2; //blue
+    //     } else if (cube.x == 180 && cube.z == 0 ||
+    //         cube.x == 0 && cube.z == 180) {
+    //         iValue = 3; // red
+    //     } else if (cube.x == 180 && cube.z == 180 ||
+    //         cube.x == 0 && cube.z == 0) {
+    //         iValue = 4; //green
+    //     } else if (cube.x == 90) {
+    //         iValue = 5; // yellow
+    //     } else if (cube.x == 0 && cube.z == 270 ||
+    //         cube.x == 180 && cube.z == 90) {
+    //         iValue = 6; // purple
+    //     }
+
+    //     UpperSideTxt = iValue;
+    //     return iValue;
+    // }
+
 
 }
