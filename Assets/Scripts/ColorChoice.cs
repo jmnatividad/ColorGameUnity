5d1f8b5791d7be061b77b13673f98a6867ac7552
Jeric Marx R. Natividad <jericmarxnatividad@gmail.com>
2024-08-05 12:48:28 +0800

    Fix for the bern copy color choice not included

--- Assets/Scripts/ColorChoice.cs
+++ Assets/Scripts/ColorChoice.cs
@@ -9,6 +9,7 @@ public class ColorChoice : MonoBehaviour
     // Start is called before the first frame update
     public GameObject[] betColor;
     public BetManager betManagerVar;
+    public ColorChoiceAudio colorChoiceAudioVar;
     public Dictionary<string, bool> currentColorSelected = new Dictionary<string, bool>{
         {"Yellow", false},
         {"White", false},
@@ -34,24 +35,28 @@ public class ColorChoice : MonoBehaviour
         if (currentColorSelected[getColor(index)])
         {
             // Revert the clicked button to its default image
-            betColor[index].GetComponent<Image>().color = new Color(1f,1f,1f,1f);
+            betColor[index].GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
             currentColorSelected[getColor(index)] = false;
+            colorChoiceAudioVar.playClickColor(false);
         }
         else
         {
-            if(betManagerVar.bet<betManagerVar.balance){
-                betColor[index].GetComponent<Image>().color = new Color(1f,1f,1f,0.5f);
+            if (betManagerVar.bet < betManagerVar.balance)
+            {
+                betColor[index].GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.5f);
                 currentColorSelected[getColor(index)] = true;
+                colorChoiceAudioVar.playClickColor(true);
             }
-        }        
+        }
     }
-    private string getColor(int index){
-        if(index==0) return "Yellow";
-        if(index==1) return "White";
-        if(index==2) return "Pink";
-        if(index==3) return "Blue";
-        if(index==4) return "Red";
-        if(index==5) return "Black";
+    private string getColor(int index)
+    {
+        if (index == 0) return "Yellow";
+        if (index == 1) return "White";
+        if (index == 2) return "Pink";
+        if (index == 3) return "Blue";
+        if (index == 4) return "Red";
+        if (index == 5) return "Black";
         return "";
     }
     public void maxButtonClick()
@@ -64,11 +69,14 @@ public class ColorChoice : MonoBehaviour
         {
             currentColorSelected[color] = true;
         }
-        
 
-        if(betManagerVar.bet<=betManagerVar.balance){
+
+        if (betManagerVar.bet <= betManagerVar.balance)
+        {
             setColor();
-        }else{
+        }
+        else
+        {
             foreach (var color in keys)
             {
                 currentColorSelected[color] = false;
@@ -78,7 +86,7 @@ public class ColorChoice : MonoBehaviour
     // check if balance allows for quick pick
     public void quickPickClick()
     {
-        
+
         // Debug.Log()
         // Create a list of keys from the dictionary
         var keys = new List<string>(currentColorSelected.Keys);
@@ -93,29 +101,35 @@ public class ColorChoice : MonoBehaviour
             keys[i] = keys[j];
             keys[j] = temp;
         }
-        
+
         // Set at least three entries to true
         for (int i = 0; i < 3; i++)
         {
             currentColorSelected[keys[i]] = true;
         }
-        if(betManagerVar.bet<=betManagerVar.balance){
+        if (betManagerVar.bet <= betManagerVar.balance)
+        {
             setColor();
-        }else{
+        }
+        else
+        {
             for (int i = 0; i < 3; i++)
             {
                 currentColorSelected[keys[i]] = false;
             }
             betManagerVar.calculateBet();
         }
-        
+
     }
 
-    public void setColor(){
-        int tempIndex=0;
-        foreach(var kvp in currentColorSelected){
-            if(kvp.Value){
-                betColor[tempIndex].GetComponent<Image>().color = new Color(1f,1f,1f,0.5f);
+    public void setColor()
+    {
+        int tempIndex = 0;
+        foreach (var kvp in currentColorSelected)
+        {
+            if (kvp.Value)
+            {
+                betColor[tempIndex].GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.5f);
             }
             tempIndex++;
         }
