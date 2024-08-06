379c03c8b1c2592b45ca3bfbe60f3a4eaebef5ce
Jeric Marx R. Natividad <jericmarxnatividad@gmail.com>
2024-08-06 15:15:52 +0800

    Revert "Merge branch 'james-branch' into jamestoJM"
    This reverts commit 4019cec30032a7dc37f3a4a30f8930ebf22c8526, reversing
    changes made to ef2c34b3a3bd5e38a9fd2a1db820a4a806eaf3fa.

--- Assets/Scripts/SFX/ChipChoiceAudio.cs
+++ Assets/Scripts/SFX/ChipChoiceAudio.cs
@@ -7,8 +7,6 @@ public class ChipChoiceAudio : MonoBehaviour
     public AudioSource source;
     public List<AudioClip> chip;
     public List<AudioClip> buttonClick;
-    public AudioClip chipExit;
-    public BetManager betManagerVar;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +18,6 @@ public class ChipChoiceAudio : MonoBehaviour
     {
         int ind = (int)Mathf.Floor(Random.Range(0, chip.Count));
         source.PlayOneShot(chip[ind], 2f);
-    }
-
-    public void playChipExit()
-    {
-        if (betManagerVar._bet != 0)
-        {
-            source.PlayOneShot(chipExit, 4f);
-        }
     }
 
     public void playButtonClick(bool State)
