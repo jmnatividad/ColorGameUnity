c8ca353e1b54982c06f7a74b460afe2a958caec6
Jeric Marx R. Natividad <jericmarxnatividad@gmail.com>
2024-08-07 12:18:11 +0800

    no message

--- Assets/Scripts/Congratulation.cs
+++ Assets/Scripts/Congratulation.cs
@@ -13,11 +13,27 @@ public class Congratulation : MonoBehaviour
 
     public void congratsWinningMoney(bool state){
         if(betManagerVar.win > 0){
-           winNumberText.text = $"{betManagerVar.win}";
+            // winNumberText.text = $"{betManagerVar.win}";
             congratulationMiddleSection.SetActive(state);
+            StartCoroutine(countToTargetNum(betManagerVar.win));
             Debug.Log($"YOU WON: ${betManagerVar.win}"); 
         }
         
+    }
+    IEnumerator countToTargetNum(int targetNum){
+        float time = 0f;
+        int currentNumber = 0;
+
+        while (time < 3.0f)
+        {
+            time += Time.deltaTime;
+            // the countup for the targetnum
+            currentNumber = Mathf.FloorToInt(Mathf.Lerp(0, targetNum, time / 3.0f));
+            winNumberText.text = currentNumber.ToString();
+            yield return null;
+        }
+        winNumberText.text = targetNum.ToString();
+
     }
 
     
