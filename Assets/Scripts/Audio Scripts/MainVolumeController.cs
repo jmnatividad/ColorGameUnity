198e664e7c358cb743cf7ee0e74f5be264e6f366
jameslunatic (office) <jameslunatic21@gmail.com>
2024-08-09 15:25:01 +0800

    Added Volume Controller + UI Placeholder

---
+++ Assets/Scripts/Audio Scripts/MainVolumeController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MainVolumeController : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mainMixer;
+
+    public void SetVolume(float sliderValue)
+    {
+        mainMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void MuteToggle(bool state)
+    {
+        if (state == false)
+        {
+            mainMixer.SetFloat("MasterVolume", Mathf.Log10(1) * 20);
+        }
+        else
+        {
+            mainMixer.SetFloat("MasterVolume", Mathf.Log10(0.0001f) * 20);
+        }
+    }
+}
